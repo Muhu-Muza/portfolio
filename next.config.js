091127b6b754bd957d6237deefedf97c7a4924eb
Muhumuza Ivan <schumannivan@gmail.com>
2023-03-15 09:48:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['drive.google.com'], ['portfolio-muhu-muza.vercel.app'], ['portfolio-git-main-muhu-muza.vercel.app'], ['portfolio-chi-henna-57.vercel.app']
+    domains: [
+              'drive.google.com', 
+              'portfolio-muhu-muza.vercel.app',
+              'portfolio-git-main-muhu-muza.vercel.app', 
+              'portfolio-chi-henna-57.vercel.app'
+             ],
   },
 }
 
