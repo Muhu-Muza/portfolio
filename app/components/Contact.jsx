9c9e83972c3caff97937a6a0c58a0b29b1387459
Muhumuza Ivan <schumannivan@gmail.com>
2024-04-17 00:38:27 +0300

    Update Contact.jsx

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import { FaGithub, FaLinkedinIn, FaEnvelope, FaFacebook } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 import Image from "next/image";
+import { Link as ScrollLink } from "react-scroll";
 
 const Contact = () => {
 
@@ -145,11 +146,17 @@ const Contact = () => {
         </div>
 
         <div className="flex justify-center py-12">
-            <Link href="/">
+                <ScrollLink
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={150}
+                >
             <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
                     <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30} />
                 </div>
-            </Link>
+            </ScrollLink>
         </div>
       </div>
     </div>
